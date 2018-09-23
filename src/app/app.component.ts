2d84d69a9605d217d8332c6d3737dd069cb4f4e6
Martin Boháč <bohacc@seznam.cz>
2018-09-23 21:24:54 +0200

    next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,35 @@ import {SwUpdate} from '@angular/service-worker';
 export class AppComponent {
   title = 'my-pwa';
   public update: boolean;
+  public deferredPrompt;
 
   constructor(private updates: SwUpdate) {
     this.updates.available
       .subscribe(() => {
         this.update = true;
+      });
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      console.log('beforeinstallprompt');
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+  }
+
+  public showConfirm() {
+    console.log(this.deferredPrompt);
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
       });
   }
 
